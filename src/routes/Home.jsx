25d41cf6202d8a94d5f16da82f159a31051639b4
unknown <subhamparidamain@gmail.com>
2023-04-14 20:41:27 +0530

    slide1 commit

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -3,6 +3,9 @@ import LandingPage from '../pages/LandingPage/LandingPage';
 import Login from "../pages/login/Login";
 import Register from "../pages/Register/Register";
 import {Routes,Route} from "react-router-dom";
+import Footer from '../component/Footer/Footer'
+import HomeTop from '../component/HomeTop/HomeTop'
+import Timeline from '../component/Timeline/Timeline'
 
 const Home = () => {
   return (

826658936558fc1a9a320c7250316a0d0f02e448
unknown <subhamparidamain@gmail.com>
2023-04-14 23:19:09 +0530

    Home page responsive done

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
-import Navbar from '../../component/navbar/Navbar'
+// import Navbar from '../../component/navbar/Navbar'
 import Homepage from '../../component/slide4/Homepage'
+import HomeTop from '../../component/HomeTop/HomeTop'
+import Footer from '../../component/Footer/Footer'
+import Timeline from '../../component/Timeline/Timeline'
 
 const LandingPage = () => {
   return (
     <div>
-      <Navbar />
+      <HomeTop/>
+      <Timeline/>
       <Homepage/>
+      <Footer/>
       </div>
   )
 }

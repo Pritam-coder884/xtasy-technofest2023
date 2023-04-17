252bb92bafead676b70f690e342a9c53bb3167d3
Shuvam Kumar Panda <shuvamkumar2001@gmail.com>
2023-04-17 10:52:29 +0530

    remove profile page from landing page

--- src/component/slide4/Homepage.jsx
+++ src/component/slide4/Homepage.jsx
@@ -60,7 +60,6 @@ const Homepage = () => {
           </div>
         </div>
       </div>
-      {/* <Sponsors/> */}
     </>
   );
 };
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,21 +1,20 @@
-import React from 'react'
-import Homepage from '../../component/slide4/Homepage'
+import React from "react";
+import Homepage from "../../component/slide4/Homepage";
 import HomeTop from "../../component/HomeTop/HomeTop";
 import TimeLine from "../../component/Timeline/Timeline";
 import Footer from "../../component/Footer/Footer";
-import Profile from '../../component/Profile';
+import Profile from "../../component/Profile";
 
 const LandingPage = () => {
   return (
     <div>
-     
       <HomeTop />
       <TimeLine />
-      <Homepage/>
+      <Homepage />
       <Footer />
-      <Profile/>
+      {/* <Profile/> */}
     </div>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;

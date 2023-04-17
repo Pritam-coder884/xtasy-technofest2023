b3b304603369dc7dd5b40319e69f21a72a6a1ad6
Shuvam Kumar Panda <shuvamkumar2001@gmail.com>
2023-04-17 10:41:21 +0530

    remove sponsor component

--- src/component/slide4/Homepage.jsx
+++ src/component/slide4/Homepage.jsx
@@ -1,59 +1,68 @@
-import React from 'react'
-import "./Homepage.scss"
+import React from "react";
+import "./Homepage.scss";
 import clockImg from "../../Assets/images/clock.png";
-import Img from "../../Assets/images/demo.png"
-import Sponsors from './Sponsors';
+import Img from "../../Assets/images/demo.png";
+import Sponsors from "./Sponsors";
 const Homepage = () => {
   return (
     <>
-    <div className='container'>
-        <div className='partOne'>
-            <div className='headingEvents'>
-                EXCITNG EVENTS FOR YOU
-            </div>
-            <div className='content'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque tenetur ipsa est. Sit, debitis consequatur.</div>
-            <div className="clock">
-            <img
-            src={clockImg}
-            alt=""
-            className="clockimg"
-          />
-            </div>
+      <div className="container">
+        <div className="partOne">
+          <div className="headingEvents">EXCITNG EVENTS FOR YOU</div>
+          <div className="content">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque
+            tenetur ipsa est. Sit, debitis consequatur.
+          </div>
+          <div className="clock">
+            <img src={clockImg} alt="" className="clockimg" />
+          </div>
         </div>
-        <div className='partTwo'>
-            <div className='cardContainer'>
-            <div className='eventCards' style={{backgroundImage:`url()`, backgroundSize:"cover"}}>
-                    <div className='cardText'>
-                        <div>Fun</div>
-                        <div>Events</div>
-                    </div>
-                </div>
-                <div className='eventCards' style={{backgroundImage:`url()`, backgroundSize:"cover"}}>
-                <div className='cardText'>
-                        <div>Fun</div>
-                        <div>Events</div>
-                    </div>
-                </div>
+        <div className="partTwo">
+          <div className="cardContainer">
+            <div
+              className="eventCards"
+              style={{ backgroundImage: `url()`, backgroundSize: "cover" }}
+            >
+              <div className="cardText">
+                <div>Fun</div>
+                <div>Events</div>
+              </div>
             </div>
-            <div className='cardContainer'>
-                <div className='eventCards' style={{backgroundImage:`url())`, backgroundSize:"cover"}}>
-                <div className='cardText'>
-                        <div>Fun</div>
-                        <div>Events</div>
-                    </div>
-                </div>
-                <div className='eventCards' style={{backgroundImage:`url()`, backgroundSize:"cover"}}>
-                <div className='cardText'>
-                        <div>Fun</div>
-                        <div>Events</div>
-                    </div>
-                </div>
+            <div
+              className="eventCards"
+              style={{ backgroundImage: `url()`, backgroundSize: "cover" }}
+            >
+              <div className="cardText">
+                <div>Fun</div>
+                <div>Events</div>
+              </div>
             </div>
+          </div>
+          <div className="cardContainer">
+            <div
+              className="eventCards"
+              style={{ backgroundImage: `url())`, backgroundSize: "cover" }}
+            >
+              <div className="cardText">
+                <div>Fun</div>
+                <div>Events</div>
+              </div>
+            </div>
+            <div
+              className="eventCards"
+              style={{ backgroundImage: `url()`, backgroundSize: "cover" }}
+            >
+              <div className="cardText">
+                <div>Fun</div>
+                <div>Events</div>
+              </div>
+            </div>
+          </div>
         </div>
-    </div>
-    <Sponsors/>
+      </div>
+      {/* <Sponsors/> */}
     </>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;

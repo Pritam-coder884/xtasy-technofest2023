3ea91100d8cee02fb16cdc5c23c6c877d87c57e8
Pritam kumar dash <pritamkumar72002@gmail.com>
2023-04-19 10:47:27 +0530

    fix issues

--- src/pages/Events/Events.jsx
+++ src/pages/Events/Events.jsx
@@ -65,10 +65,10 @@ const Events = (props) => {
       <div className="event-cards-1">
         <div className="all-1">
           <div className="cards-1">
-            {(eventsNow?eventsNow : technical).map((event,index) => {
+            {(eventsNow).map((event,index) => {
+              {console.log(eventsNow)}
               return (
-              
-                <EventCards key={event.id} name={event.eventName} desc={event} handleClick={handleClick} img={props.text === 'TECHNICAL EVENTS' ? event.id : 35}/>)
+                <EventCards key={event.id} name={event.eventName} desc={event} handleClick={handleClick} img={event.id}/>)
             
             })}
           </div>
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Homepage from "../../component/slide4/Homepage";
 import HomeTop from "../../component/HomeTop/HomeTop";
 import TimeLine from "../../component/Timeline/Timeline";
 import Footer from "../../component/Footer/Footer";
 import Profile from "../../component/ProfileTop/ProfileTop";
+import axios from "axios";
 
 const LandingPage = () => {
-
+  // useEffect(() => axios.get("http://43.205.194.46:4000/api/event/getAllEvents").then((data)=>console.log(data)),[])
   return (
     <div>
       <HomeTop />

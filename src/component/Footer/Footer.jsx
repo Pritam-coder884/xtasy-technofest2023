4b572e7a9e3bef605823181c2dae73e2f734cd35
Shuvam Kumar Panda <shuvamkumar2001@gmail.com>
2023-04-19 14:07:08 +0530

    change contact details in footer

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -1,24 +1,34 @@
-import React from 'react'
-import './Footer.scss'
+import React from "react";
+import "./Footer.scss";
 
 const Footer = () => {
   return (
     <div>
       <div className="footer">
         <div className="left">
-            <h2>For further details contact : </h2>
-            <p className="row">
-                <p className="lefty">xtasy.2023@outr.in</p>
-                <p className="righty">7684811743</p>
-            </p>
+          <h2>For further details contact : </h2>
+          <p className="row">
+            <p className="lefty">xtasy.2023@outr.in</p>
+            <p className="righty">Rasmi: 9348651129</p>
+            <p className="righty">Pratyush: 7894239471</p>
+          </p>
         </div>
-        <div className="right">
-            <p>Designed & devoloped with ❤️</p>
-            <p>by <a href='https://instagram.com/zairza.outr?igshid=YmMyMTA2M2Y=' target='_blank' className='zairza'>zairza</a></p>
-        </div>
+        {/* <div className="right">
+          <p>Designed & devoloped with ❤️</p>
+          <p>
+            by{" "}
+            <a
+              href="https://instagram.com/zairza.outr?igshid=YmMyMTA2M2Y="
+              target="_blank"
+              className="zairza"
+            >
+              zairza
+            </a>
+          </p>
+        </div> */}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
